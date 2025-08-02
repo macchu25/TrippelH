efc39b1506f1a73d137d75d0051d96cee9dd02b2
Shridhan <shridhan.suman2023@vitstudent.ac.in>
2025-08-02 22:09:48 +0530

    login using clerk

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -262,7 +262,7 @@ export const dummyMessagesData = [
         "createdAt": "2025-07-25T10:44:12.753Z",
         "updatedAt": "2025-07-25T10:44:12.753Z",
     },
-        {
+    {
         "_id": "6878cc2817a54e4d3748010c",
         "from_user_id": "user_2zdFoZib5lNr614LgkONdD8WG32",
         "to_user_id": "user_2zwZSCMRXQ9GaEEVLgm6akQo96i",
