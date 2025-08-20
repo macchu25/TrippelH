2d7e633eb4786a20406d132aa5713d3da0ee0e48
Shridhan <shridhan.suman2023@vitstudent.ac.in>
2025-08-20 12:38:00 +0530

    story viewer component

--- client/src/components/StoryModal.jsx
+++ client/src/components/StoryModal.jsx
@@ -1,5 +1,6 @@
 import { ArrowLeft, Sparkle, TextIcon, Upload } from "lucide-react";
 import React, { useState } from "react";
+import toast from "react-hot-toast";
 
 const StoryModal = ({ setShowModal, fetchStories }) => {
   const bgColors = [
@@ -90,7 +91,11 @@ const StoryModal = ({ setShowModal, fetchStories }) => {
             </label>
 
         </div>
-        <button className=" cursor-pointer flex items-center justify-center gap-2 text-white py-3 mt-4 w-full rounded bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-600 hover:to-purple-700 active:scale-95 transition" onClick={handleCreateStory}>
+        <button onClick={()=> toast.promise(handleCreateStory(),{
+          loading: 'Saving...',
+          success: <p>Story Added</p>,
+          error: e=> <p>{e.message} </p>
+        })} className=" cursor-pointer flex items-center justify-center gap-2 text-white py-3 mt-4 w-full rounded bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-600 hover:to-purple-700 active:scale-95 transition" >
            <Sparkle size={18} /> Create Story
         </button>
       </div>
