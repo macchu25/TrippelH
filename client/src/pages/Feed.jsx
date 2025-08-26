00de668e28be1bd65f0653e292e15eae729a2433
Shridhan <shridhan.suman2023@vitstudent.ac.in>
2025-08-26 17:38:39 +0530

    recent messages component

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { dummyPostsData } from '../assets/assets'
+import { assets, dummyPostsData } from '../assets/assets'
 import Loading from '../components/Loading'
 import StoriesBar from '../components/StoriesBar'
 import PostCard from '../components/PostCard'
+import RecentMessages from '../components/RecentMessages'
 
 const Feed = () => {
 
@@ -33,12 +34,16 @@ const Feed = () => {
 
 
     {/* right sidebar */}
-    <div>
+    <div className='max-xl:hidden sticky top-0'>
 
-      <div>
-        <h1>Sponsored</h1>
+      <div className='max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow'>
+        <h3 className='text-slate-800 font-semibold'>Sponsored</h3>
+        <img src={assets.sponsored_img} className='w-75 h-50 rounded-md' alt="" />
+        <p className='text-slate-600'>Email Marketin</p>
+        <p className='text-slate-400'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est minus reiciendis beatae eum obcaecati et ullam aspernatur similique magni quisquam.</p>
       </div>
-      <h1>Recent Messages</h1>
+      
+      <RecentMessages />
 
 
     </div>
