d9a410ad155aa37dcd5cd5c6a90a3e5fa04b1fed
Shridhan <shridhan.suman2023@vitstudent.ac.in>
2025-08-31 20:28:48 +0530

    clerk webhooks

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,7 +1,68 @@
 import { Inngest } from "inngest";
+import User from "../models/User.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "pingup-app" });
 
+// Inngest fuction to save user to database 
+const syncUserCreation=inngest.createFunction(
+    {id:'sync-user-from-clerk'},
+    {event:'clerk/user.created'},
+    async({event})=>{
+        const {id,first_name,last_name,email_addresses,image_url}=event.data;
+        let username = email_addresses[0].email_address.split('@')[0];
+        // check availability of user last_name
+        const user = await User.findOne({ user_name: username });
+        if(user){
+            username = username + Math.floor(Math.random() * 1000);
+        }
+        const userData = {
+            _id: id,
+            email: email_addresses[0].email_address,
+            full_name: first_name + " " + last_name,
+            profile_picture: image_url, 
+            username
+        }
+
+        await User.create(userData);
+    }
+)
+
+
+// Ingest function to update user in database
+const syncUserUpdation = inngest.createFunction(
+    { id: 'update-user-from-clerk' },
+    { event: 'clerk/user.updated' },
+    async ({ event }) => {
+        const { id, first_name, last_name, email_addresses, image_url } = event.data;
+
+        const updatedUserData={
+            email: email_addresses[0].email_address,
+            full_name: first_name + " " + last_name,
+            profile_picture: image_url
+        }
+
+        await User.findByIdAndUpdate(id, updatedUserData);
+    }
+);
+
+
+// Ingest function to delete user from database
+const syncUserDeletion = inngest.createFunction(
+    { id: 'delete-user-with-clerk' },
+    { event: 'clerk/user.deleted' },
+    async ({ event }) => {
+        const { id } = event.data;
+
+        await User.findByIdAndDelete(id);
+    }
+);
+
+ 
+
 // Create an empty array where we'll export future Inngest functions
-export const functions = [];
+export const functions = [
+    syncUserCreation,
+    syncUserUpdation,
+    syncUserDeletion
+];
