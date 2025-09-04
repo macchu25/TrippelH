e58ad7ef4cc3b25c19281a721948fae81735e0ae
Shridhan <shridhan.suman2023@vitstudent.ac.in>
2025-09-05 02:31:47 +0530

    post model

---
+++ server/models/Post.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const postSchema = new mongoose.Schema({
+
+    user: { type: String, ref: 'User', required: true },
+    content: { type: String, required: true },
+    image_urls: [{ type: String }],
+    post_type: { type: String, enum: ['text', 'image', 'video'], required: true },
+    likes_count: [{ type: String, ref: 'User' }],
+
+}, { timestamps: true, minimize: false })
+
+const Post = mongoose.model('Post', postSchema);
+export default Post;
