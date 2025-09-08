e1b87d78fd232147e5bbb92f2fb35291d7c8eb2b
Shridhan <shridhan.suman2023@vitstudent.ac.in>
2025-09-08 14:53:16 +0530

    create story function

--- server/models/Story.js
+++ server/models/Story.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const storySchema = new mongoose.Schema({
 
     user: { type: String, ref: 'User', required: true },
-    content: { type: String, required: true },
+    content: { type: String},
     media_url: { type: String },
     media_type: { type: String, enum: ['text', 'image', 'video'] },
     view_count: [{ type: String, ref: 'User' }],
