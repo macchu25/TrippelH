27e7d1fc41541a4e1de7ec2c18fbe6a9f3eb9d4a
macchu25 <daylahuu@gmail.com>
2025-10-19 14:49:56 +0700

    b

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,9 +1,9 @@
 import { Inngest } from "inngest";
 import User from "../models/User.js";
 import Connection from "../models/Connection.js";
-// import sendEmail from "../configs/nodeMailer.js";
-// import Story from "../models/Story.js";
-// import Message from "../models/Messages.js";
+import sendEmail from "../configs/nodeMailer.js";
+import Story from "../models/Story.js";
+import Message from "../models/Messages.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "pingup-app" });
